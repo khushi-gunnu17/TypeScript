2ee48f7272fb09ddfb662cd313dc94f8133be550
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-07-08 23:46:06 +0530

    comments updation

--- 00_intro.ts
+++ 00_intro.ts
@@ -10,3 +10,7 @@ console.log(user.name);
 // console.log(user.email);
 
 export {}
+
+// In TypeScript, any file containing top-level import or export statements is considered a module. Modules have their own scope and do not pollute the global scope. Conversely, files without import or export statements are treated as scripts, and their contents are available in the global scope.
+
+// export {} is a way to ensure a TypeScript file is treated as a module, providing scope encapsulation and preventing global namespace pollution. This is useful even if there are no actual exports in the file at the moment.
